20dbb34d9bddea5e2b2bfbd914f855f6d6ba192c
AcidicSoil <claytonbivens1@gmail.com>
2025-03-26 17:15:14 -0500

    4th cmt

--- startMcpdoc.ps1
+++
@@ -1,7 +0,0 @@
- uvx --from mcpdoc mcpdoc     --urls LangGraph:https://langchain-ai.github.io/langgraph/llms.txt     --transport sse     --port 8082     --host localhost
-
-
-
- # starts the llm router for cursor ollama runs instead of default
- # cursor (claude models)
- # ./llm-router-windows-amd64.exe -log-level debug -port 11434
